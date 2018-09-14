572c9d6042a7f0f1afd45927121ea3053b31d5f4
MrTj458 <mrtj458@gmail.com>
2018-09-14 14:19:40 -0600

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import TodoForm from './components/TodoForm'
+import Todo from './components/Todo'
+
+class App extends React.Component {
+  state = {todos: []}
+  
+  componentDidMount() {
+    fetch('/api/items')
+      .then(res => res.json())
+      .then(todos => this.setState({todos: todos}))
+  }
+
+  addItem = (name) => {
+    const item = {name}
+    fetch('/api/items', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(item)
+    }).then(res => res.json())
+      .then(todo => {
+        const {todos} = this.state
+        this.setState({todos: [todo, ...todos]})
+      })
+  }
+
+  updateItem = (id) => {
+    fetch(`/api/items/${id}`, {method: 'PUT'})
+      .then(res => res.json())
+      .then(todo => {
+        const todos = this.state.todos.map(t => {
+          if(t.id === id) {
+            return todo
+          }
+          return t
+        })
+        this.setState({todos: todos})
+      })
+  }
+
+  deleteItem = (id) => {
+    fetch(`/api/items/${id}`, {method: 'DELETE'})
+      .then(() => {
+        const { todos } = this.state
+        this.setState({ todos: todos.filter(t => t.id !== id) })
+      })
+  }
+
+  todoList = () => {
+    return this.state.todos.map(todo => {
+      return (
+        <div className="row">
+          <Todo
+            {...todo}
+            updateItem={this.updateItem}
+            deleteItem={this.deleteItem}
+          />
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <TodoForm addItem={this.addItem} />
+          {this.todoList()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
